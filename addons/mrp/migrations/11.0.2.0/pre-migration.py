6a3621394e5076efa978f318c0c0b0c0c8b75bb6
Adrià Gil Sorribes <adria.gil@eficent.com>
2019-11-27 10:39:27 +0100

    [11.0][ADD] hobbii-customization

--- addons/mrp/migrations/11.0.2.0/pre-migration.py
+++ addons/mrp/migrations/11.0.2.0/pre-migration.py
@@ -12,6 +12,25 @@ _column_renames = {
 }
 
 
+def compute_done_move(cr):
+    openupgrade.logger.debug("Computation of done_move in stock_move_line")
+    cr.execute(
+        """
+        ALTER TABLE stock_move_line ADD COLUMN done_move boolean;
+        """
+    )
+    openupgrade.logged_query(
+        cr,
+        """
+        UPDATE stock_move_line AS to_update_sml
+        SET done_move = sm.is_done
+        FROM stock_move AS sm
+        WHERE sm.id = to_update_sml.move_id;        
+        """
+    )
+
+
 @openupgrade.migrate(use_env=False)
 def migrate(cr, version):
+    compute_done_move(cr)
     openupgrade.rename_columns(cr, _column_renames)
