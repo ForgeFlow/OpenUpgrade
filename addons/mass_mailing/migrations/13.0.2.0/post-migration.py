852b41394b88305b78dda47a8d55f21879bb35a2
mreficent <miquel.raich@eficent.com>
2021-02-24 19:27:41 +0100

    [OU-FIX] mass_mailing: update mailing_domain by using customer_rank/supplier_rank

--- addons/mass_mailing/migrations/13.0.2.0/post-migration.py
+++ addons/mass_mailing/migrations/13.0.2.0/post-migration.py
@@ -2,6 +2,25 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
 
+
+def update_mailing_domain_filter(env):
+    if openupgrade.table_exists(env.cr, 'account_invoice'):
+        openupgrade.logged_query(
+            env.cr, """
+            UPDATE mailing_mailing
+            SET mailing_domain = replace(mailing_domain,
+                '''customer''', '''customer_rank''')
+            WHERE mailing_domain LIKE '%''customer''%'""",
+        )
+        openupgrade.logged_query(
+            env.cr, """
+            UPDATE mailing_mailing
+            SET mailing_domain = replace(mailing_domain,
+                '''supplier''', '''supplier_rank''')
+            WHERE mailing_domain LIKE '%''supplier''%'""",
+        )
+
+
 _unlink_by_xmlid = [
     # mail.mass_mailing.list
     'mass_mailing.mass_mail_list_1',
@@ -11,4 +30,5 @@ _unlink_by_xmlid = [
 @openupgrade.migrate(use_env=True)
 def migrate(env, version):
     openupgrade.delete_records_safely_by_xml_id(env, _unlink_by_xmlid)
+    update_mailing_domain_filter(env)
     openupgrade.load_data(env.cr, 'mass_mailing', 'migrations/13.0.2.0/noupdate_changes.xml')
