9e94a36b3c3730c866f231e27e1e9da0182d2b91
Miquel Raïch <miquel.raich@forgeflow.com>
2022-10-05 06:37:37 +0200

    [IMP] openupgrade_framework: add enterprise_upgrade_scripts

--- openupgrade_framework/__init__.py
+++ openupgrade_framework/__init__.py
@@ -14,3 +14,12 @@ if not config.get("upgrade_path"):
             "location of openupgrade_scripts"
         )
         config["upgrade_path"] = os.path.join(path, "scripts")
+        path = get_module_path("enterprise_upgrade_scripts", display_warning=False)
+        if path:
+            logging.getLogger(__name__).info(
+                "Setting upgrade_path to the scripts directory inside the module "
+                "location of enterprise_upgrade_scripts"
+            )
+            config["upgrade_path"] = (
+                config["upgrade_path"] + "," + os.path.join(path, "scripts")
+            )
