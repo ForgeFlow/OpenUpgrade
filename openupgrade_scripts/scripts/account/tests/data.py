9f65cebbfafcb221c9f3a665eb5fd31dd6cf10c7
Holger Brunn <mail@hunki-enterprises.com>
2025-05-05 12:06:53 +0200

    fixup! fixup! fixup! [OU-ADD] account

--- openupgrade_scripts/scripts/account/tests/data.py
+++ openupgrade_scripts/scripts/account/tests/data.py
@@ -2,7 +2,13 @@ env = locals().get("env")
 # call sending wizard on some moves asynchronously
 action = (
     env["account.move"]
-    .search([("move_type", "=", "out_invoice")])
+    .search(
+        [
+            ("move_type", "=", "out_invoice"),
+            ("state", "=", "posted"),
+            ("company_id", "=", env.ref("base.main_company").id),
+        ]
+    )
     .action_send_and_print()
 )
 env[action["res_model"]].with_context(**action["context"]).create(
