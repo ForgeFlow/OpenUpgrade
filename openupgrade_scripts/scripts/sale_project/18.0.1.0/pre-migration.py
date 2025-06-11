3304ff4e5d40a6a1a06e5d91f9a68cca64c09620
Rémi - Le Filament <30716308+remi-filament@users.noreply.github.com>
2025-06-11 13:26:00 +0200

    [OU-ADD] sale_project

---
+++ openupgrade_scripts/scripts/sale_project/18.0.1.0/pre-migration.py
@@ -0,0 +1,30 @@
+# Copyright 2025 Le Filament (https://le-filament.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from openupgradelib import openupgrade
+
+
+def _fill_project_reinvoiced_sale_order_id(env):
+    openupgrade.logged_query(
+        env.cr,
+        """
+         ALTER TABLE project_project ADD COLUMN
+         IF NOT EXISTS reinvoiced_sale_order_id INTEGER
+         """,
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE project_project p
+        SET reinvoiced_sale_order_id = sol.order_id
+        FROM sale_order_line sol
+        WHERE p.reinvoiced_sale_order_id IS NULL
+        AND p.sale_line_id IS NOT NULL
+        AND p.sale_line_id = sol.id
+        """,
+    )
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    _fill_project_reinvoiced_sale_order_id(env)
